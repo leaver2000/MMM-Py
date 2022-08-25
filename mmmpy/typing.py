e8cfa1447a121fbd838ff9ebcc3cc8d64fef8968
TSgt Jason Leaver <jason.leaver@us.af.mil>
2022-08-25 13:05:34 +0000

    .

--- mmmpy/typing.py
+++ mmmpy/typing.py
@@ -5,3 +5,4 @@ from typing import TypeVar, Literal
 StrPath = TypeVar("StrPath", str, Path)
 Engine = Literal["netcdf4", "cfgrib", "binary"]
 Archive = Literal["zip", "tar", "gztar", "bztar", "xztar"]
+
